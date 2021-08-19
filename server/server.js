ea874d4b3a00b7e19100c5c4600072fc8f7c4253
georgiedekker <georgiedekker@gmail.com>
2021-08-19 23:55:17 +0200

    changed CORS

--- server/server.js
+++ server/server.js
@@ -12,12 +12,12 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true }))
 const url = process.env.DB_URL
 const port = process.env.PORT || 3000
-app.use(cors({origin: ['http://localhost:'+port, 'http://localhost:8080', url, 'cors-anywhere.herokuapp.com:'+port, 'cors-anywhere.herokuapp.com:8080', 'https://611ebaf485726653be7bab67--eloquent-lalande-af32e9.netlify.app:8080','https://611ebaf485726653be7bab67--eloquent-lalande-af32e9.netlify.app:'+port],
+app.use(cors({origin: ['http://localhost:'+port, 'http://localhost:8080', url, 'cors-anywhere.herokuapp.com:'+port, 'cors-anywhere.herokuapp.com:8080', 'https://611ed10c61ab8361c03cf96b--eloquent-lalande-af32e9.netlify.app/:8080','https://611ed10c61ab8361c03cf96b--eloquent-lalande-af32e9.netlify.app:'+port],
 optionsSuccessStatus: 200 }));
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: ["http://192.168.2.6:8080","http://localhost:8080"],
+    origin: ["http://192.168.2.6:8080","http://localhost:8080", "https://611ed10c61ab8361c03cf96b--eloquent-lalande-af32e9.netlify.app:8080","https://611ed10c61ab8361c03cf96b--eloquent-lalande-af32e9.netlify.app"],
     methods: ["GET", "POST"],
     allowedHeaders: ["Access-Control-Allow-Origin"],
     credentials: true
