e4dd08c06040a2da6357dcc23e3f3ac3f16f469d
georgiedekker <georgiedekker@gmail.com>
2021-08-21 11:52:07 +0200

    changed CORS

--- server/server.js
+++ server/server.js
@@ -17,7 +17,11 @@ optionsSuccessStatus: 200 }));
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin:"https://611ed10c61ab8361c03cf96b--eloquent-lalande-af32e9.netlify.app:8080",
+    origin:["https://611ed10c61ab8361c03cf96b--eloquent-lalande-af32e9.netlify.app:8080",
+                "http:localhost:8080",
+              "https://vue3chatsocketio.herokuapp.com/",
+            "https://eloquent-lalande-af32e9.netlify.app/",
+            "https://eloquent-lalande-af32e9.netlify.app:8080"],
     methods: ["GET", "POST"],
     allowedHeaders: ["Access-Control-Allow-Origin"],
     credentials: true
